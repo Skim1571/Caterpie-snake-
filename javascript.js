5907d5a45c804ac6aee1ce275231c402dcd1f6b1
Skim1571 <skim1571@gmail.com>
2022-07-05 14:52:53 -0500

    reworked board. could not get class object to work the way I wanted it to.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 ////////////Global Variables////////////
 const boardSection = document.getElementById('board-grid')
 const tileArr = []
-const tileObj = {}
-// for in loop
-// need a unique key to 'mimic' a key ; call on the tiles by using something like tileObj[0]
-
+const caterpiePosition = []
+let size =  prompt(`'what is the  board size?`)
 
 // Init Game
 
@@ -12,12 +10,18 @@ const tileObj = {}
 // Caterpie Classes
 class Caterpie {
   constructor(){
+this.id = snake
   }
+
 }
 
 class Head extends Caterpie{
   constructor(){
     super()
+    this.position =  caterpiePosition.push(Math.ceil(Math.pow(size,2))/2)
+  }
+  eat(){
+  //when eventListener triggers, (head goes into position where berry is), then the body will replicate and append to the head.
   }
 }
 
@@ -25,18 +29,27 @@ class Body extends Caterpie {
   constructor(){
     super()
   }
+  createBody(){
+//create a new body part.
+
+  }
+  follow(){
+//needs to follow the part that's 1 position ahead
+  }
 }
 
 class Tail extends Caterpie {
   constructor(){
     super()
   }
+  //tail needs to follow the last body part
 }
 
 
 ////////////functions////////////
+
+//function to create board
 const createBoard = (size) => {
-let sqAmount = parseInt(size, 10)
   for (let i = 0; i < Math.pow(size,2); i++){
     let div = document.createElement(`div`)
     div.setAttribute('class','sq')
@@ -47,6 +60,11 @@ let sqAmount = parseInt(size, 10)
   boardSection.style.gridTemplateRows = `repeat(${size}, minmax(25px, 25px))`;
   }
 
-createBoard(3)
+//function to create caterpie
+const createCaterpie = () => {
+  const head = new Head()
+}
+
+createBoard(parseInt(size))
 
 ////////////event listeners////////////
